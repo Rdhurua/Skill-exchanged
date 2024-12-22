34b26af71a05fd941de12f6514ff8c51c650f0b6
Rinku Dhurua <rinkudhurua258@gmail.com>
2024-12-22 09:43:56 +0530

    json error resolved

--- server/config/mongoose-connection.js
+++ server/config/mongoose-connection.js
@@ -1,19 +1,15 @@
-const mongoose=require("mongoose");
-const dbgr=require("debug")("development:mongoose");
-const config=require("config");
-const dotenv=require("dotenv");
-  dotenv.config();
+require("dotenv").config(); // Load .env file
+const mongoose = require("mongoose");
+
+const mongoURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGODB_URL_PRODUCTION // Use production URL in production mode
+    : process.env.MONGODB_URL_LOCAL; // Use local URL in other environments
+
 mongoose
-.connect(`${config.get(process.env.MONGODB_URL2 || process.env.MONGODB_URL)}/skill-exchanged`,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-.then(function(){
-    dbgr("connected to database");
-})
-.catch(function(err){
-     dbgr(err);
-})
-
-module.exports=mongoose.connection
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Connected to MongoDB at ${mongoURL}`))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
