324fe60cf2d5b449d5e261a05c2b6d9d0a22c2c6
Rinku Dhurua <rinkudhurua258@gmail.com>
2024-12-22 09:34:26 +0530

    json error resolved

--- server/config/mongoose-connection.js
+++ server/config/mongoose-connection.js
@@ -4,7 +4,7 @@ const config=require("config");
 const dotenv=require("dotenv");
   dotenv.config();
 mongoose
-.connect(`${config.get(process.env.MONGODB_URL2 || process.env.MONGODB_URL)}/skill-exchanged`,{
+.connect(`${config.get("MONGODB_URL2" || "MONGODB_URL")}/skill-exchanged`,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
