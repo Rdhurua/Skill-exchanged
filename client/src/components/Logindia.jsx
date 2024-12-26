746fcaec39f69d26a024e367f58078394efea089
Rinku Dhurua <rinkudhurua258@gmail.com>
2024-12-26 11:10:28 +0530

    error resolving

--- client/src/components/Logindia.jsx
+++ client/src/components/Logindia.jsx
@@ -88,7 +88,7 @@ const Logindia = ({ value,handle}) => {
       // localStorage.setItem("userData", JSON.stringify(userdata.user));
   
       // Navigate to the user profile page and pass the data
-      navigate(`/userProfile/${userdata.id}`);
+      navigate(`/userProfile/${userdata.user._id}`);
     } catch (error) {
       console.error('Error fetching user data:', error.response?.data || error.message);
     }
